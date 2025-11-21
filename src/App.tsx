2eeaebc10e0a3f09a4d2d60151b49fa344dddf33
nikivault <nikithasudeer@gmail.com>
2025-11-21 20:45:30 +0530

    chore: initial commit - Vite + React + Tailwind + Chakra base

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+﻿export default function App() {
+  return (
+    <div style={{ padding: "20px", fontSize: "20px" }}>
+      <h1>Elysien Website</h1>
+      <p>Your React + Vite project is now running!</p>
+    </div>
+  );
+}
